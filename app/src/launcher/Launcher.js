cad4ec05bd08fa95a8c3e770e866d2e2a80010c7
Damodar1996 <dhegde50@gmail.com>
2019-03-24 23:21:02 +0530

    Launcher changed to stateless component

--- app/src/launcher/Launcher.js
+++ app/src/launcher/Launcher.js
@@ -1,16 +1,15 @@
 import React from "react";
 import "./Launcher.scss";
 
-class Launcher extends React.Component {
-    render() {
-        return (
-            <div className="template">
-                <h1>Project launched!</h1>
-                <h2>Happy coding!</h2>
-            </div>
+const Launcher = () => {
+    return (
 
-        );
-    }
-};
+        <div className="template">
+            <h1>Project launched!</h1>
+            <h2>Happy coding!</h2>
+        </div>
+
+    );
+}
 
 export default Launcher;
