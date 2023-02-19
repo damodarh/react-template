b68cb5119536a6b0244cceaf714dc935616ac538
Damodar Hegde <dhegde50@gmail.com>
2023-02-19 16:47:55 -0600

    Added css file  processing

---
+++ src/App/App.jsx
@@ -0,0 +1,5 @@
+import './App.css';
+
+const App = () => <h1 className='text'>Hello from React!</h1>;
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
 module.exports = {
@@ -11,13 +12,16 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.join(__dirname, 'public', 'index.html'),
     }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+    }),
   ],
   devServer: {
     static: {
       directory: path.join(__dirname, 'build'),
     },
     port: 3000,
-    open: true
+    open: true,
   },
   module: {
     rules: [
@@ -25,6 +29,10 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: ['babel-loader'],
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
     ],
   },
